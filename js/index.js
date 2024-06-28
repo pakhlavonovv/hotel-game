4c1e4318d03c89dff9e961fb2b24ff9128e8161e
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-06-28 08:34:42 +0500

    hotel-game

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let narx = +prompt("Nechi pullik hona sizga ma'qul?")
+if (narx === 1000){
+    alert("Siz lux honadan joy oldingiz!!!")
+} else if (narx >= 500){
+    alert("Siz yarim lux honadan joy oldingiz")
+} else if (narx < 200){
+    alert("Siz oddiy honadan joy oldingiz")
+}
